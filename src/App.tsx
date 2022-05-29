b813c7917b08860425db16e72c1c48bc6f3c04ba
Aliya <saigafarova00@yandex.ru>
2022-05-30 02:27:51 +0300

    fix: linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 
-function App () {
+function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.tsx</code> and save to reload.
+            Edit
+            {' '}
+            <code>src/App.tsx</code>
+            {' '}
+            and save to reload.
           </p>
           <a
             className="App-link"
