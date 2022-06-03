1d32031f02b509a8e4793b492ab4b4c7879f028f
Aliya <saigafarova00@yandex.ru>
2022-06-03 03:37:46 +0300

    Updates

--- src/components/CatLoader.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const CatLoader = () => {
-  return(
-      <span>... загружаем еще котиков ...</span>
-  );
-};
--- src/constants/paths.router.ts
+++
@@ -1,2 +0,0 @@
-export const ALL_CATS_PATH = '/';
-export const FAVORITES_CATS_PATH = '/favorites';
